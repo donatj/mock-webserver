401cf06889c3616bd95ca20b308d63d07dc432ce
Vitor Mattos <vitor@php.rio>
2021-05-31 11:16:02 -0300

    Reduce sleep to make tests more fast

--- src/MockWebServer.php
+++ src/MockWebServer.php
@@ -86,7 +86,12 @@ class MockWebServer {
 			throw new Exceptions\ServerException("Error starting server, received '{$this->pid}', expected int PID");
 		}
 
-		sleep(1); // just to make sure it's fully started up, maybe not necessary
+		for ($i=0; $i<=2;$i++) {
+			if( $this->isRunning() ) {
+				break;
+			}
+			usleep(500000); // just to make sure it's fully started up, maybe not necessary
+		}
 
 		if( !$this->isRunning() ) {
 			throw new Exceptions\ServerException("Failed to start server. Is something already running on port {$this->port}?");
