1fde5ecb816ca05465eef1a258129b35e5dd369a
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-09-01 20:15:40 +0200

    Add #[ReturnTypeWillChange] for PHP 8.1 support
    In PHP 8.1 the return type of JsonSerializable::jsonSerialize() was added.
    To maintain compatibility with older PHP versions the attribute
    #[ReturnTypeWillChange] was introduced.
    
    Deprecation warning: Return type of donatj\MockWebServer\RequestInfo::jsonSerialize()
    should either be compatible with JsonSerializable::jsonSerialize(): mixed, or the
    #[ReturnTypeWillChange] attribute should be used to temporarily suppress the notice
    
    See also: https://wiki.php.net/rfc/internal_method_return_types
    
    As the package phpspec/prophecy (a dependency from phpunit) has not released yet a
    new version with PHP 8.1 compatibility I added a switch for composer install for
    the time being.

--- src/RequestInfo.php
+++ src/RequestInfo.php
@@ -85,6 +85,7 @@ class RequestInfo implements \JsonSerializable {
 	 * which is a value of any type other than a resource.
 	 * @since 5.4.0
 	 */
+	#[\ReturnTypeWillChange]
 	public function jsonSerialize() {
 		return [
 			self::JSON_KEY_GET          => $this->get,
